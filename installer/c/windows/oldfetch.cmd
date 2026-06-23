5d6f291188d997aa4ef8f626c460a65c71019622
KrypZero <KrypZero@Proton.ME>
2026-06-23 19:40:44 +0000

    Create oldfetch.cmd

---
+++ installer/c/windows/oldfetch.cmd
@@ -0,0 +1,14 @@
+:: WARNING
+::This project is editable, You care what Windows version you using
+
+
+@echo off
+echo ::::::::  ::::::::      <user>-<hostname>
+echo ::::::::  ::::::::      ----------
+echo ::::::::  ::::::::      OS: <windowsversion>
+echo ::::::::  ::::::::      Shell: <cmd>
+echo                         DE: explorer.exe
+echo ::::::::  ::::::::      WM: Desktop Window Manager
+echo ::::::::  ::::::::      Icon Theme: <icontheme>
+echo ::::::::  ::::::::       ----------
+echo ::::::::  ::::::::

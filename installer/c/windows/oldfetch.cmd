570791cad4bcb4ae30218687e05d8e471840d174
KrypZero <KrypZero@Proton.ME>
2026-06-24 13:29:04 +0000

    Update oldfetch.cmd

--- installer/c/windows/oldfetch.cmd
+++ installer/c/windows/oldfetch.cmd
@@ -1,8 +1,9 @@
-:: WARNING
-:: This project is editable, You care what Windows version you using
-:: If wish you want remove, type pre-added command 'remove-oldfetch'
+@echo off
+::WARNING
+::This project is editable, You care what Windows version you using
+::If wish you want remove, type pre-added command 'remove-oldfetch'
 
-:: My configs:
+::My configs:
 ::echo                 KrypZero@WindowsXP
 ::echO                 --------------
 ::echo                 Host: HP TC1100
@@ -17,18 +18,17 @@
 ::echo                 Icon Theme: Recycle Bin
 ::echo                 Terminal: Commmand Prompt
 
-:: This is default config use:
-@echo off
-echo :::::::::  :::::::::    <user>-<hostname>
+::This is default config use:
+echo :::::::::  :::::::::    [user]-[hostname]
 echo :::::::::  :::::::::    ----------
-echo :::::::::  :::::::::    OS: <windowsversion>
-echo :::::::::  :::::::::    Shell: <cmd>
+echo :::::::::  :::::::::    OS: [windowsversion]
+echo :::::::::  :::::::::    Shell: [cmd]
 echo                         DE: explorer.exe
 echo :::::::::  :::::::::    WM: Desktop Window Manager
-echo :::::::::  :::::::::    Icon Theme: <icontheme>
+echo :::::::::  :::::::::    Icon Theme: [icontheme]
 echo :::::::::  :::::::::    ----------
-echo :::::::::  :::::::::    Host: <yourpc>
-echo                         CPU: <yourcpu>
-echo                         GPU: <yourgpu>
-:: Please note that you have a 2 or more GPUs
-echo                         RAM: <ramsize>
+echo :::::::::  :::::::::    Host: [yourpc]
+echo                         CPU: [yourcpu]
+echo                         GPU: [yourgpu]
+::Please note that you have a 2 or more GPUs
+echo                         RAM: [ramsize]

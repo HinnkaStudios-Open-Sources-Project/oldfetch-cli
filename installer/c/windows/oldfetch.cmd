8e4fcdcb677917e2fdb2e94a9ee4330fb1e5d82a
KrypZero <KrypZero@Proton.ME>
2026-06-24 09:36:11 +0000

    Update oldfetch.cmd

--- installer/c/windows/oldfetch.cmd
+++ installer/c/windows/oldfetch.cmd
@@ -1,14 +1,18 @@
 :: WARNING
 ::This project is editable, You care what Windows version you using
-
+:: This is default config use
 
 @echo off
-echo ::::::::  ::::::::      <user>-<hostname>
-echo ::::::::  ::::::::      ----------
-echo ::::::::  ::::::::      OS: <windowsversion>
-echo ::::::::  ::::::::      Shell: <cmd>
+echo :::::::::  :::::::::    <user>-<hostname>
+echo :::::::::  :::::::::    ----------
+echo :::::::::  :::::::::    OS: <windowsversion>
+echo :::::::::  :::::::::    Shell: <cmd>
 echo                         DE: explorer.exe
-echo ::::::::  ::::::::      WM: Desktop Window Manager
-echo ::::::::  ::::::::      Icon Theme: <icontheme>
-echo ::::::::  ::::::::       ----------
-echo ::::::::  ::::::::
+echo :::::::::  :::::::::    WM: Desktop Window Manager
+echo :::::::::  :::::::::    Icon Theme: <icontheme>
+echo :::::::::  :::::::::    ----------
+echo :::::::::  :::::::::    Host: <yourpc>
+echo                         CPU: <yourcpu>
+echo                         GPU: <yourgpu>
+:: Please note that you have a 2 or more GPUs
+echo                         RAM: <ramsize>

ecd95183369e49a16bd25b062ffe8ef697dea21c
KrypZero <KrypZero@Proton.ME>
2026-06-24 09:42:40 +0000

    Update oldfetch.cmd

--- installer/c/windows/oldfetch/oldfetch.cmd
+++ installer/c/windows/oldfetch/oldfetch.cmd
@@ -1,1 +1,19 @@
+:: WARNING
+:: This project is editable, You care what Windows version you using
+:: This is default config use
+:: If wish you want remove, type pre-added command 'remove-oldfetch'
 
+@echo off
+echo :::::::::  :::::::::    <user>-<hostname>
+echo :::::::::  :::::::::    ----------
+echo :::::::::  :::::::::    OS: <windowsversion>
+echo :::::::::  :::::::::    Shell: <cmd>
+echo                         DE: explorer.exe
+echo :::::::::  :::::::::    WM: Desktop Window Manager
+echo :::::::::  :::::::::    Icon Theme: <icontheme>
+echo :::::::::  :::::::::    ----------
+echo :::::::::  :::::::::    Host: <yourpc>
+echo                         CPU: <yourcpu>
+echo                         GPU: <yourgpu>
+:: Please note that you have a 2 or more GPUs
+echo                         RAM: <ramsize>

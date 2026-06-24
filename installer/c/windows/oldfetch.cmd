8a331d058f23b9c2f873f2669a601929546f1cc6
KrypZero <KrypZero@Proton.ME>
2026-06-24 12:31:31 +0000

    Update oldfetch.cmd

--- installer/c/windows/oldfetch.cmd
+++ installer/c/windows/oldfetch.cmd
@@ -1,8 +1,23 @@
 :: WARNING
 :: This project is editable, You care what Windows version you using
-:: This is default config use
 :: If wish you want remove, type pre-added command 'remove-oldfetch'
 
+:: My configs:
+::echo                 KrypZero@WindowsXP
+::echO                 --------------
+::echo                 Host: HP TC1100
+::echo    ::    ::     CPU: Intel Pentium M
+::echO    ::    ::     GPU: NVIDIA GeForce4 Go 420
+::echo    ::    ::     RAM: 768MB
+::echo                 --------------
+::echo                 OS: Windows XP Professional x86
+::echo ::              Shell: CMD
+::echo ::              DE: explorer.exe
+::echo ::...........   WM: Desktop Windows Manager
+::echo                 Icon Theme: Recycle Bin
+::echo                 Terminal: Commmand Prompt
+
+:: This is default config use:
 @echo off
 echo :::::::::  :::::::::    <user>-<hostname>
 echo :::::::::  :::::::::    ----------
